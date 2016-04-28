d9291e26ed61d3f62014783e563f71d54f5a3ed5
Chen Zack <czktc2007@gmail.com>
2016-04-28 20:43:10 +0800

    use 3 linear-layout to hold sub elements

--- src/xziar/mylesson/view/actionbar/SimpleActionBar.java
+++ src/xziar/mylesson/view/actionbar/SimpleActionBar.java
@@ -3,11 +3,16 @@ package xziar.mylesson.view.actionbar;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class SimpleActionBar extends RelativeLayout
 {
+	private LinearLayout BarLeft, BarMid, BarRight;
+
 	public SimpleActionBar(Context context)
 	{
 		super(context);
@@ -28,12 +33,34 @@ public class SimpleActionBar extends RelativeLayout
 
 	private void init(Context context)
 	{
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		BarLeft = new LinearLayout(context, null);
+		BarLeft.setLayoutParams(lp);
+		BarLeft.setOrientation(LinearLayout.HORIZONTAL);
+		RelativeLayout.LayoutParams parmL = new RelativeLayout.LayoutParams(lp);
+		parmL.addRule(RelativeLayout.ALIGN_PARENT_START);
+		super.addView(BarLeft, -1, parmL);
+		
+		BarRight = new LinearLayout(context, null);
+		BarRight.setLayoutParams(lp);
+		BarRight.setOrientation(LinearLayout.HORIZONTAL);
+		RelativeLayout.LayoutParams parmR = new RelativeLayout.LayoutParams(lp);
+		parmR.addRule(RelativeLayout.ALIGN_PARENT_END);
+		super.addView(BarRight, -1, parmR);
+		
+		BarMid = new LinearLayout(context, null);
+		BarMid.setLayoutParams(lp);
+		BarMid.setOrientation(LinearLayout.HORIZONTAL);
+		RelativeLayout.LayoutParams parmM = new RelativeLayout.LayoutParams(lp);
+		parmM.addRule(RelativeLayout.CENTER_IN_PARENT);
+		super.addView(BarMid, -1, parmM);
+		
 		Log.v("tester", "ActBar from ctx:" + context.getClass().getName());
 	}
 
 	@Override
-	public void addView(View child, int index,
-			android.view.ViewGroup.LayoutParams params)
+	public void addView(View child, int index, ViewGroup.LayoutParams params)
 	{
 		Log.v("tester", "----- addView,view,idx,param");
 		Class<?>[] viewifs = child.getClass().getInterfaces();
@@ -48,21 +75,20 @@ public class SimpleActionBar extends RelativeLayout
 		}
 		if (!isAdd)
 			return;
-		ActionBarElement abe = (ActionBarElement)child;
-		RelativeLayout.LayoutParams parm = (RelativeLayout.LayoutParams) params;
-		switch(abe.getAlign())
+		ActionBarElement abe = (ActionBarElement) child;
+		switch (abe.getAlign())
 		{
 		case center:
-			parm.addRule(RelativeLayout.CENTER_IN_PARENT);
+			BarMid.addView(child, -1, params);
 			break;
 		case left:
-			parm.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+			BarLeft.addView(child, -1, params);
 			break;
 		case right:
-			parm.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+			BarRight.addView(child, 0, params);
 			break;
 		}
-		super.addView(child, index, params);
+		//super.addView(child, index, params);
 	}
 
 }
